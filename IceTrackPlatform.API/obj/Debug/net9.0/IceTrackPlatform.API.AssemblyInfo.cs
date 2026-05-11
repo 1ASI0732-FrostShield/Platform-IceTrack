f8b85bf27eac3489e4232cbe3edba0c4959b057a
Julio Guillen <u20241a352@upc.edu.pe>
2026-05-10 22:27:45 -0500

    Actualizada

--- IceTrackPlatform.API/obj/Debug/net9.0/IceTrackPlatform.API.AssemblyInfo.cs
+++ IceTrackPlatform.API/obj/Debug/net9.0/IceTrackPlatform.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IceTrackPlatform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ccf8feb6e19fc629f8a6972517aa347490979e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5fd9e9655f62078da24a129e3e5477885b8df65")]
 [assembly: System.Reflection.AssemblyProductAttribute("IceTrackPlatform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IceTrackPlatform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
